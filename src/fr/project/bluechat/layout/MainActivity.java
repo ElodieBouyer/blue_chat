6db77222027a4f6b113b22d8211d08ede2eeea7b
Elo <elodie.bouyer@hotmail.fr>
2014-07-07 10:40:57 +0200

    Package creation.

--- src/fr/project/bluechat/layout/MainActivity.java
+++ src/fr/project/bluechat/layout/MainActivity.java
@@ -1,5 +1,9 @@
-package fr.project.bluechat;
+package fr.project.bluechat.layout;
 
+import fr.project.bluechat.R;
+import fr.project.bluechat.R.id;
+import fr.project.bluechat.R.layout;
+import fr.project.bluechat.R.menu;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
