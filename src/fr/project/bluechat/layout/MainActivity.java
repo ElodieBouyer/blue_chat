d9eeeef6be6cd8d7c7ddeef9f48b30976822fe1a
Elo <elodie.bouyer@hotmail.fr>
2014-07-07 14:37:24 +0200

    Add edit menu.

--- src/fr/project/bluechat/layout/MainActivity.java
+++ src/fr/project/bluechat/layout/MainActivity.java
@@ -1,5 +1,6 @@
 package fr.project.bluechat.layout;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -12,6 +13,7 @@ import fr.project.bluechat.R;
 
 public class MainActivity extends FragmentActivity {
 
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,24 +22,22 @@ public class MainActivity extends FragmentActivity {
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 			.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		} 
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+
+		switch (item.getItemId()) {
+
+		case R.id.action_edit:
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
