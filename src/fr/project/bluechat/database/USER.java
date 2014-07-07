dbf207e95a67f02dc0ce7596b83bbf762ea29410
Elo <elodie.bouyer@hotmail.fr>
2014-07-07 16:40:53 +0200

    Bug fix USER.java.

--- src/fr/project/bluechat/database/USER.java
+++ src/fr/project/bluechat/database/USER.java
@@ -1,108 +1,16 @@
 package fr.project.bluechat.database;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
+class USER {
+	static final String TAB_USER = "USER";
 
-public class User {
-	private Database database;
-	private SQLiteDatabase db;
-	private String name = null;
+	static final String COL_NAME      = "USER_NAME";
 
-	/**
-	 * User Constructor.
-	 * Create database if not exist.
-	 * @param context Context's application.
-	 */
-	public User(Context context) {
-		database = new Database(context);
-	}
+	static final int NUM_COL_NAME      = 0;
 
-	/**
-	 * Opens the database in writable, 
-	 * and put its content into db.
-	 */
-	public void open() {
-		db = database.getWritableDatabase();
-	}
+	static final String[] ALL_COLUMNS = {COL_NAME };
 
-	/**
-	 * Close access to the database db.
-	 */
-	public void close() {
-		db.close();
-	}
-
-
-	/**
-	 * Get the user's name.
-	 * @return User's name.
-	 */
-	public String getName() {
-		try {
-			if( name != null ) return name;
-
-			open();
-			Cursor c = this.db.query(
-					USER.TAB_USER,                  
-					USER.ALL_COLUMNS,                
-					null,
-					null, null, null, null, null);
-
-			if( c.getCount() == 0) {
-				return null;
-			}
-
-			c.moveToFirst();
-			name = c.getString(USER.NUM_COL_NAME);
-			close();
-			return name;
-
-		}
-		catch(Exception e) {
-			Log.i("User.getName()" , e.toString());
-		}
-		return name;
-	}
-
-	/**
-	 * Set the user's name.
-	 * @param name
-	 */
-	public void setName(String newName) {
-		if( newName == null) return;
-
-		try {
-			ContentValues values = new ContentValues();
-			delete();
-			name = newName;
-
-			open();
-			values.put(USER.COL_NAME, name);
-			db.insert(USER.TAB_USER, null,values);
-			close();
-		}
-		catch(Exception e) {
-			Log.i("User.setName", e.toString());
-		}
-	}
-
-
-	/**
-	 * Delete name.
-	 */
-	private void delete() {
-		if( name == null ) return;
-
-		try {
-			open();
-			db.delete(USER.TAB_USER, USER.COL_NAME + " LIKE \"" + name + "\"" , null);
-			close();
-		}
-		catch(Exception e) {
-			Log.i("User.delete", e.toString());
-		}
-	}
+	static final String CREATE_TABLE_USER = 
+			"CREATE TABLE " 
+					+ TAB_USER  + " (" 
+					+ COL_NAME  + " TEXT NOT NULL UNIQUE);";
 }
