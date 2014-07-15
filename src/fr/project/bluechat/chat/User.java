87892a596d428d469859fdc354d4089b02068d79
Elo <elodie.bouyer@hotmail.fr>
2014-07-15 15:30:16 +0200

    Remove warnings.

--- src/fr/project/bluechat/chat/User.java
+++ src/fr/project/bluechat/chat/User.java
@@ -1,47 +1,41 @@
 package fr.project.bluechat.chat;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.UUID;
 
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothClass.Device;
-import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.os.Bundle;
 
 
 public class User{
-	
+
 	private String name = null;
 	private BluetoothSocket btSocket =null; 
 	private BluetoothAdapter mAdapter= BluetoothAdapter.getDefaultAdapter();
 	public User(String name){
 		this.name=name;
-	
+
 	}
-	
+
 	public String getName(){
 		return this.name;
 	}
-	
+
 	public void SetName(String name){
 		this.name=name;
 	}
-	
+
 	public void sendMessage(String name, String message){
-		
+
 	}	
-	
+
 	public void connect(){
-		
+
 		if (btSocket != null) {
-            if(mAdapter.isDiscovering()) {
-            	try {
-            		btSocket.connect();
-            	} catch (IOException e) {}
-            }
+			if(mAdapter.isDiscovering()) {
+				try {
+					btSocket.connect();
+				} catch (IOException e) {}
+			}
 		}
 	}
 }
